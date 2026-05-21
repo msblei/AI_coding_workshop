ab3a5d164a756880f1e0cb6102475e0685d9ad84
Marius Bleif <marius.bleif@t-online.de>
2026-05-21 17:29:34 +0200

    update reset_app

--- scripts/reset-app.sh
+++ scripts/reset-app.sh
@@ -11,8 +11,10 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>✅ Everything works!</h1>
+      <h1>Everything works!</h1>
       <p>Wait for instructions from the workshop leaders.</p>
+      <p>If there have been any changes, refresh this browser window.</p>
+
     </div>
   );
 }
